58cd720adc1ab1f1662a8594b27c42001d9b192e
warren <artful.tittle.0o@gmail.com>
2025-06-15 12:24:31 +0900

    변수,상수 작성 과제

---
+++ JS/Variable-declaration+Constant declaration.js
@@ -0,0 +1,20 @@
+const NAME = "naruto"
+
+const ABSOLUTE_ZERO = "-273.15"
+
+let loginOn = "true"
+
+let age = "30"
+
+let price = "39_900원"
+
+const PAGE_BACKGROUND = "white"
+
+let commentsCount = 12
+
+let currentPage = 3
+
+let membershipLevel = "VIP"
+
+let isClick = "true"
+
